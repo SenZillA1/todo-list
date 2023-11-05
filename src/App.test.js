dd93e68e466c64433945cfc68873f657e60fcb27
SenZillA1 <91287731+SenZillA1@users.noreply.github.com>
2023-11-05 12:38:24 +0100

    upadte

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders To Do List header', () => {
+    render( < App / > );
+    const headerElement = screen.getByText(/learn react/i);
+    expect(headerElement).toBeInTheDocument();
 });
